ea5092c10256be5e4e9fbe9b39c1aabc21249b54
koenc100 <koen604@gmail.com>
2021-06-14 15:29:08 +0200

    move dataset to code file

--- code/data_prep.py
+++ code/data_prep.py
@@ -1,1 +1,12 @@
 # Data preparation: Strokes project
+
+# import libraries
+import numpy as np
+import pandas as pd
+import math
+import statistics
+
+
+data = pd.read_csv('Project-AI-minor/healthcare-dataset-stroke-data.csv')
+
+#print(data.head())
