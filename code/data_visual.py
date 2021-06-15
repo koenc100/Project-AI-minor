5e953c45d79c73ffda18ce7d3450c9700392115a
bunny115 <jana.bersee@student.uva.nl>
2021-06-15 12:24:55 +0200

    a few changes

--- code/data_visual.py
+++ code/data_visual.py
@@ -1,1 +1,8 @@
-# visualize the data 
+# visualize the data
+
+from data_prep import prepare_data
+
+data, empty_data, labels_data, empty_labels = prepare_data('healthcare-dataset-stroke-data.csv', split_size=(1, 0))
+
+# print(data.info())
+print(labels_data)
