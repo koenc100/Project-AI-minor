8d1b87dcd51188e7bcdbdc47a1f599b553cd7edf
koenc100 <koen604@gmail.com>
2021-06-16 15:10:54 +0200

    final improvements

--- code/data_visual.py
+++ code/data_visual.py
@@ -25,37 +25,37 @@ data = prepare_data('healthcare-dataset-stroke-data.csv')
 # Create subplot
 fig, axes = plt.subplots(2, 3, figsize=(20,10))
 
-fig.suptitle('The strokes or nonstrokes for age, average glucose level and bmi ')
+fig.suptitle('The strokes or nonstrokes for age, average glucose level and bmi')
 
 # Add 3 histogram plots to the subplot
 sns.histplot(ax=axes[0, 0], x=data['age'], hue=data['stroke'], bins=30, kde=True)
-axes[0, 0].set_title('Age for every stroke')
+axes[0, 0].set_title('Age per stroke / non-stroke')
 axes[0, 0].set_xlabel('Age')
 axes[0, 0].set_ylabel('Stroke count')
 
 sns.histplot(ax=axes[0, 1], x=data['avg_glucose_level'], hue=data['stroke'], bins=30, kde=True)
-axes[0, 1].set_title('Average glucose level for every stroke')
-axes[0, 1].set_xlabel('Average glucose level')
+axes[0, 1].set_title('Average glucose level per stroke / non-stroke')
+axes[0, 1].set_xlabel('Average glucose level (mg/DL)')
 
 sns.histplot(ax=axes[0, 2], x=data['bmi'], hue=data['stroke'], bins=30, kde=True)
-axes[0, 2].set_title('bmi for every stroke')
+axes[0, 2].set_title('bmi per stroke / non-stroke')
 axes[0, 2].set_xlabel('bmi')
 
-# Add 3 proportional histograms to the subpots
+# Add 3 cumulative density histograms to the subpots
 sns.histplot(ax=axes[1, 0], x=data['age'], hue=data['stroke'], element="step", fill=False,
     cumulative=True, stat="density", common_norm=False,)
-axes[1, 0].set_title('Age for every stroke')
+axes[1, 0].set_title('cumulative histogram of age vs stroke / non-stroke')
 axes[1, 0].set_xlabel('Age')
-axes[1, 0].set_ylabel('Stroke count')
+axes[1, 0].set_ylabel('Density')
 
 sns.histplot(ax=axes[1, 1], x=data['avg_glucose_level'], hue=data['stroke'], element="step", fill=False,
     cumulative=True, stat="density", common_norm=False,)
-axes[1, 1].set_title('Average glucose level for every stroke')
-axes[1, 1].set_xlabel('Average glucose level')
+axes[1, 1].set_title('cumulative histogram of avg. gl. level vs stroke / non-stroke')
+axes[1, 1].set_xlabel('Average glucose level (mg/DL)')
 
 sns.histplot(ax=axes[1, 2], x=data['bmi'], hue=data['stroke'], element="step", fill=False,
     cumulative=True, stat="density", common_norm=False,)
-axes[1, 2].set_title('bmi for every stroke')
+axes[1, 2].set_title('cumulative histogram of bmi vs stroke / non-stroke ')
 axes[1, 2].set_xlabel('bmi')
 
 plt.show()
