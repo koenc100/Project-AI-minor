501ac9df4d20cbace0950e313c5629ea21d39de5
jeroendijkmans <jeroendijkmans@hotmail.com>
2021-06-17 13:23:53 +0200

    get_metrics and DNN updated

--- code/helper_functions.py
+++ code/helper_functions.py
@@ -1,5 +1,4 @@
-from sklearn.metrics import balanced_accuracy_score, accuracy_score, confusion_matrix, specificity, sensitivity
-
+from sklearn.metrics import balanced_accuracy_score, accuracy_score, confusion_matrix
 
 def get_metrics(true_labels, predictions, verbose=False):
     """
@@ -9,7 +8,7 @@ def get_metrics(true_labels, predictions, verbose=False):
     accuracy = accuracy_score(true_labels, predictions)
     balanced_accuracy = balanced_accuracy_score(true_labels, predictions)
     conmat = confusion_matrix(true_labels, predictions)
-    sensitivity = conmat[1,1] / sum(conmat[1,:]
+    sensitivity = conmat[1,1] / sum(conmat[1,:])
     specificity = conmat[0,0] / sum(conmat[0,:])
 
     if verbose is True:
