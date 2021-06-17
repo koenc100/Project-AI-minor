cb524632c2cb34b768fbac1217817d8ec90b7b55
jeroendijkmans <jeroendijkmans@hotmail.com>
2021-06-17 13:18:42 +0200

    get_metrics updated

--- code/helper_functions.py
+++ code/helper_functions.py
@@ -1,4 +1,5 @@
-from sklearn.metrics import balanced_accuracy_score, accuracy_score, confusion_matrix
+from sklearn.metrics import balanced_accuracy_score, accuracy_score, confusion_matrix, specificity, sensitivity
+
 
 def get_metrics(true_labels, predictions, verbose=False):
     """
@@ -7,14 +8,19 @@ def get_metrics(true_labels, predictions, verbose=False):
     """
     accuracy = accuracy_score(true_labels, predictions)
     balanced_accuracy = balanced_accuracy_score(true_labels, predictions)
+    conmat = confusion_matrix(true_labels, predictions)
+    sensitivity = conmat[1,1] / sum(conmat[1,:]
+    specificity = conmat[0,0] / sum(conmat[0,:])
 
     if verbose is True:
         print(f'accuracy: {accuracy*100:.4f} % \n')
 
-        print(f'balanced accuracy: {balanced_accuracy*100:.4f} % \n')
+        print(f'balanced accuracy: {balanced_accuracy*100:.4f} %')
+        print(f'sensitivity: {sensitivity}')
+        print(f'specificity: {specificity} \n')
 
         print('confusion matrix: ')
-        print(f'{confusion_matrix(true_labels, predictions)} \n')
+        print(f'{conmat} \n')
         print('[["True Negative", "False Positive"] \n ["False Negative", "True Positive"]] \n')
 
     return accuracy, balanced_accuracy
