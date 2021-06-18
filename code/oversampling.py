b170d15704c0dffdc6c23d7ff330885328744be1
koenc100 <koen604@gmail.com>
2021-06-18 17:00:31 +0200

    finish oversampling

--- code/oversampling.py
+++ code/oversampling.py
@@ -3,7 +3,7 @@
 #import imblearn
 #from imblearn.over_sampling import RandomOverSampler, SMOTENC
 import pandas as pd
-from data_processing import prepare_data, split_data, one_hot
+from data_processing import prepare_data, split_data
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,12 +11,36 @@ import scipy
 import sklearn
 from imblearn.over_sampling import RandomOverSampler, SMOTENC
 
+def one_hot(data, columns):
 
-data = prepare_data('healthcare-dataset-stroke-data.csv', one_hot = False, binary = True, normalize = True)
+    """
+    Returns pandas dataframe with one hot encoded columns
+    data: data used
+    columns: list of strings of names of columns to one hot encode
+    """
 
+    # create new frame list
+    new_frame = [data]
 
-train_data, test_data, train_labels, test_labels, = split_data(data, split_size=(0.999, 0.001))
+    # loop over every column in column list
+    for column_name in columns:
 
+        # Create dummies objects for one-hot encoded columns
+        column_dummie = pd.get_dummies(data[column_name])
+
+        # append to list
+        new_frame.append(column_dummie)
+
+    # Drop not one-hot endcoded columns
+    data = data.drop(columns, axis=1)
+
+    # Create new dataframe with one-hot endcoded columns
+    data = pd.concat(new_frame, axis=1)
+
+    # Rename the unknown smoking status column
+    data = data.rename(columns={'Unknown':'unknown_smoking_status'})
+
+    return data
 
 def smote_loop(data, labels, n_features, start, stop, step):
 
@@ -47,25 +71,26 @@ def smote_loop(data, labels, n_features, start, stop, step):
         # create resampled data and labels
         train_data_res_t, train_labels_res = smote_nc.fit_resample(data, labels)
 
-        print(train_data_res_t.shape)
-
+        # encode one hot
         train_data_res = one_hot(train_data_res_t, ['work_type', 'smoking_status'])
 
-        print(train_data_res.shape)
-        
         # list with sampled data and  labels
         list_data.append(train_data_res)
         list_labels.append(train_labels_res)
         list_ratio.append(i)
 
-        train_data_res['stroke'] = train_labels_res
-
-        sns.histplot( x=train_data_res['age'], hue=train_data_res['stroke'], bins=30, kde=True)
-        plt.show()
-
     return list_data, list_labels, list_ratio
 
-# Define categorial features
-n_features = np.array([True, False, True, True, True, True,True, False, False, True])
 
-print(smote_loop(train_data, train_labels, n_features, 0.2, 1.1, 0.2))
+if __name__ == '__main__':
+
+    data = prepare_data('healthcare-dataset-stroke-data.csv', one_hot = False, binary = True, normalize = True)
+
+    train_data, test_data, train_labels, test_labels, = split_data(data, split_size=(0.999, 0.001))
+
+    # Define categorial features
+    n_features = np.array([True, False, True, True, True, True,True, False, False, True])
+
+    smote_test = smote_loop(train_data, train_labels, n_features, 0.2, 1.1, 0.2)
+
+    print(smote_test)
