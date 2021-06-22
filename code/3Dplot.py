ff91a71a0efd48b238f6d0fcee8b137d8df3cc90
koenc100 <koen604@gmail.com>
2021-06-22 17:23:19 +0200

    work on 3d lot

--- code/3Dplot.py
+++ code/3Dplot.py
@@ -4,29 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+import math
 
-# make figure
-fig = plt.figure()
+# make 3D figure
+fig = plt.figure(figsize=(10,10))
 
 # create 3 axises
 ax = fig.add_subplot(111, projection='3d')
 
-#The X and Y input values
-# This would be class-weight ratio and oversampling ratio
-X0 = np.array([2, 3, 4, 2.5, 1.5, 1])
-Y0 = np.array([1, 2, 3, 4, 3, 2])
+X0 = np.linspace(0, 10, 100)
+Y0 = np.linspace(0, 10, 100)
 
 # make a grid, in order to plot every point
 X, Y = np.meshgrid(X0, Y0)
 
-# This would be the outcome of NN with certain values
-Z = (X + Y)
+Z =  np.sin(X) + np.sqrt(Y)
 
+# make plot
 actual_plot = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm)
+
+# add colorbar
 fig.colorbar(actual_plot, shrink=0.5, aspect=5)
 
-ax.set_xlabel('Class weight ratio')
-ax.set_ylabel('Oversampling ratio')
+# Label X and Y
+
+ax.set_xlabel('Oversampling ratio')
+ax.set_ylabel('Class weight ratio')
 ax.set_zlabel('Performance in percentage')
 
 plt.show()
