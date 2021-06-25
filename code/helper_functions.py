6e27da8ba45103c6122ed3c90a4a9051eb8cbe54
bunny115 <jana.bersee@student.uva.nl>
2021-06-25 11:43:10 +0200

    spatie minder in get_metrics en update combined_model

--- code/helper_functions.py
+++ code/helper_functions.py
@@ -12,7 +12,7 @@ def get_metrics(true_labels, predictions, verbose=False):
     specificity = conmat[0,0] / sum(conmat[0,:])
 
     if verbose is True:
-        print(f'accuracy: {accuracy*100:.4f} % \n')
+        print(f'accuracy: {accuracy*100:.4f} %')
 
         print(f'balanced accuracy: {balanced_accuracy*100:.4f} %')
         print(f'sensitivity: {sensitivity:.4f}')
@@ -33,5 +33,5 @@ def get_confusion_metrics(true_labels, predictions):
     conmat = confusion_matrix(true_labels, predictions)
     sensitivity = conmat[1,1] / sum(conmat[1,:])
     specificity = conmat[0,0] / sum(conmat[0,:])
-    
+
     return conmat, sensitivity, specificity
